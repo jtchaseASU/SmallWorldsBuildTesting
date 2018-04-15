e9e915e21c24b9b160b445b5ea14e69d67267589
jtchaseASU <jtchase@asu.edu>
2018-04-15 15:57:09 -0700

    Test for Screen clarity and button changes

--- TemplateData/DetectWebGL.js
+++ TemplateData/DetectWebGL.js
@@ -16,9 +16,9 @@ function DetectWebGL(e, t, r) {
     if (!rendcon || !gl) {
     	if (confirm("Small Worlds cannot run because" + 
             "WebGL is not supported on your device.")) {
-    		window.location = "https://etx.asu.edu/";
+    		window.location = "https://infiniscope.org/";
     	} else {
-    		window.location = "https://etx.asu.edu/";
+    		window.location = "https://infiniscope.org/";
     	}
     }
     //calls passed function
